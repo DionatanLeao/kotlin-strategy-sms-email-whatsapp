70d4389a6936087ab4bc17ec052cfbdef0bd6717
DionatanLeao <dionatan.ti@outlook.com>
2023-09-16 22:45:34 -0300

    Created project

---
+++ src/main/kotlin/com/webcode/kotlinstrategysmsemailwhatsapp/KotlinStrategySmsEmailWhatsappApplication.kt
@@ -0,0 +1,11 @@
+package com.webcode.kotlinstrategysmsemailwhatsapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinStrategySmsEmailWhatsappApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinStrategySmsEmailWhatsappApplication>(*args)
+}
